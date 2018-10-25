cc5b57d3c7ea637bade0af35855847845cf1c00a
John Bulger <35578045+jdbul33@users.noreply.github.com>
2018-10-24 21:50:03 -0400

    more paper

--- stepwise.R
+++ stepwise.R
@@ -2,15 +2,25 @@ data <- read.csv('C:/Users/jdbul/Documents/GitHub/reimagined-parakeet/sas_ready.
 
 library(dplyr)
 library(leaps)
+library(GGally)
+library(caret)
 
 df <- data %>%
         select(-TotalHomicides, -Ward)
+
+corr_mat <- round(cor(df),2)
+#ggpairs(df)
+
+
 
 
 null <- lm(TotalCrime ~ 1, data=df)
 
 full <- lm(TotalCrime ~ ., data=df)
 
-step(null, scope=list(lower=null, upper=full), direction="forward")
-step(full, data=df, direction="backward")
-step(null, scope = list(upper=full), data=df, direction="both")
+
+
+answer3 <- step(null, scope=list(lower=null, upper=full), direction="forward")
+answer2 <- step(full, data=df, direction="backward")
+answer <- step(null, scope = list(upper=full), data=df, direction="both")
+answer2
